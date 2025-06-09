e9fcb871032dad14a8b481d66d0bc3eb9b3201da
Daniel Muhia <muhiadan17@gmail.com>
2025-06-09 14:46:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// findMatching() function
+
+function findMatching(arr, name){
+    return arr.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+
+// fuzzyMatch() function
+function fuzzyMatch(arr, letters){
+    return arr.filter(driver => driver.startsWith(letters));
+}
+
+// matchName() function
+function matchName(arr, name){
+    return arr.filter(driver => driver.name === name);
+}
